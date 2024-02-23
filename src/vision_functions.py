b4779569598f6a177a046eac46cbcaf6d59f1980
Ivan Chen <ivanchen3k@gmail.com>
2024-02-22 18:51:37 -0600

    changed hsl to hsv

--- src/vision_functions.py
+++ src/vision_functions.py
@@ -5,7 +5,7 @@ import numpy as np
 def transformation_matrix():
 	print()
 
-def hsl_filter(color_matrix, hsv_threshold, output_matrix):
+def hsv_filter(color_matrix, hsv_threshold, output_matrix):
 	cv2.inRange(color_matrix, cv2.Scalar(hsv_threshold[0], hsv_threshold[2], hsv_threshold[4]), cv2.Scalar(hsv_threshold[1], hsv_threshold[3], hsv_threshold[5]), output_matrix)
 
 def get_position(position_matrix, binary_mask):
