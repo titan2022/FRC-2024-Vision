74ba2a5c1d565f77d0bd7c1be5db0f23548e1982
Ivan Chen <ivanchen3k@gmail.com>
2024-02-22 18:50:12 -0600

    None

---
+++ src/vision_functions.py
@@ -0,0 +1,13 @@
+import math
+import cv2
+import numpy as np
+
+def transformation_matrix():
+	print()
+
+def hsl_filter(color_matrix, hsv_threshold, output_matrix):
+	cv2.inRange(color_matrix, cv2.Scalar(hsv_threshold[0], hsv_threshold[2], hsv_threshold[4]), cv2.Scalar(hsv_threshold[1], hsv_threshold[3], hsv_threshold[5]), output_matrix)
+
+def get_position(position_matrix, binary_mask):
+	return cv2.mean(position_matrix, binary_mask)
+
