8b5b5e8a99426e6572123dbbcd01a75dc5c8f12d
ethanc8 <echaroenpitaks@gmail.com>
2024-03-03 17:43:38 -0600

    Try putting a dot at the center of the bounding box (it doesn't work)

--- src/example-webcam.py
+++ src/example-webcam.py
@@ -16,7 +16,16 @@ while cap.isOpened():
     assert success
 
     res = model.track(frame, persist=True)
-    annot_frame = res[0].plot()
+    firstResult: ultralytics.engine.results.Results = res[0]
+    annot_frame = firstResult.plot()
+    boxes = firstResult.boxes.xyxy
+
+    for x1, y1, x2, y2 in boxes:
+        print(type(x1))
+        print(((x1, y1), (x2, y2)))
+        center = (int((x1.item() + x2.item())/2), int((y1.item() + y2.item())/2))
+        print(center)
+        cv2.circle(frame, center, 7, (255, 0, 0), cv2.FILLED) # Show circle
 
     cv2.imshow("Tracking", annot_frame)
 
